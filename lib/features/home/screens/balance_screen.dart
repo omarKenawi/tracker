9283ab861fc22ae5fe1cc7c5ad402ed92c551db9
omar <202027471@std.sci.cu.edu.eg>
2024-08-20 17:09:54 +0300

    add expanses & balance & settings screens

---
+++ lib/features/home/screens/balance_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BalanceScreen extends StatelessWidget {
+  const BalanceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Balance Screen'),
+    );
+  }
+}
--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,47 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:tracker/features/home/screens/settings_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'balance_screen.dart';
+import 'expenses_screen.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(() {
+      setState(() {
+        _selectedIndex = _tabController.index;
+      });
+    });
+  }
+
+  void _onDrawerItemSelected(int index) {
+    setState(() {
+      _selectedIndex = index;
+      _tabController.index = index;
+    });
+    Navigator.of(context).pop();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 50),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  ListTile(
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                    title: Text(
+                      'Expenses',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: _selectedIndex == 0 ? FontWeight.bold : FontWeight.normal,
+                      ),
+                    ),
+                    onTap: () => _onDrawerItemSelected(0),
+                  ),
+                  ListTile(
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                    title: Text(
+                      'Balance',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: _selectedIndex == 1 ? FontWeight.bold : FontWeight.normal,
+                      ),
+                    ),
+                    onTap: () => _onDrawerItemSelected(1),
+                  ),
+                  ListTile(
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                    title: Text(
+                      'Settings',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: _selectedIndex == 2 ? FontWeight.bold : FontWeight.normal,
+                      ),
+                    ),
+                    onTap: () => _onDrawerItemSelected(2),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        elevation: 0,
         backgroundColor: Colors.white,
-        drawer: const Drawer(),
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          centerTitle: true,
-          title: const Text(
-            'Overview',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-          ),
-          bottom: const TabBar(
-            indicatorColor: Colors.transparent,
-            dividerColor: Colors.white,
-            labelColor: Colors.black,
-            unselectedLabelColor: Colors.grey,
-            tabs: [
-              Tab(text: 'EXPENSES'),
-              Tab(text: 'BALANCE'),
-              Tab(text: 'SETTINGS'),
-            ],
-            labelStyle: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold
-            ),
-            unselectedLabelStyle: TextStyle(
-              fontSize: 16,
-            ),
+        centerTitle: true,
+        title: const Text(
+          'Overview',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
           ),
         ),
-
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.transparent,
+          dividerColor: Colors.white,
+          labelColor: Colors.black,
+          unselectedLabelColor: Colors.grey,
+          tabs: const [
+            Tab(text: 'EXPENSES'),
+            Tab(text: 'BALANCE'),
+            Tab(text: 'SETTINGS'),
+          ],
+          labelStyle: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+          unselectedLabelStyle: const TextStyle(
+            fontSize: 16,
+          ),
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
+          ExpensesScreen(),
+          BalanceScreen(),
+          SettingsScreen(),
+        ],
       ),
     );
   }
 }
+
