576892513662e3eab7c524f5826a33114fa2867b
omar <202027471@std.sci.cu.edu.eg>
2024-08-21 19:50:48 +0300

    add cash flow widget

---
+++ lib/features/home/widgets/cash_flow_widget.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
+import 'package:tracker/features/home/widgets/progress_bar_widget.dart';
+
+class CashFlowWidget extends StatelessWidget {
+  final int earnedAmount;
+  final int spentAmount;
+  final int balanceAmount;
+  final double earnedProgressWidth;
+  final double spentProgressWidth;
+
+  const CashFlowWidget({
+    super.key,
+    required this.earnedAmount,
+    required this.spentAmount,
+    required this.balanceAmount,
+    required this.earnedProgressWidth,
+    required this.spentProgressWidth,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final NumberFormat currencyFormat = NumberFormat('#,##0');
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'CASH FLOW',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 17.sp,
+          ),
+        ),
+        SizedBox(
+          height: 15.h,
+        ),
+        Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ProgressBarWidget(
+                  color: const Color(0xFF1450D1),
+                  progressWidth: earnedProgressWidth,
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+                ProgressBarWidget(
+                  color: Colors.red,
+                  progressWidth: spentProgressWidth,
+                ),
+              ],
+            ),
+            SizedBox(
+              width: 10.w,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '\$${currencyFormat.format(earnedAmount)} Earned',
+                  style: const TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+                Text(
+                  '\$${currencyFormat.format(spentAmount)} Spent',
+                  style: const TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            Text(
+              '\$${currencyFormat.format(earnedAmount - spentAmount)}',
+              style: TextStyle(
+                fontSize: 22.sp,
+                color: const Color(0xFF1450D1),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
